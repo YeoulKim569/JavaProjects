e0507ccd8f3bddde39c57419254b299cb0e22885
YeoulKim569 <yeoul421@gmail.com>
2026-02-20 19:32:29 +0800

    adjust code and add toDoList

--- src/todo_list_application/Main.java
+++ src/todo_list_application/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) throws ParseException {
 		MainTask mainTask = new MainTask("Study", "16/02/2026 08:28", "17/02/2026 08:28", "study math", false);
-		Subtask subTask = new Subtask("Note solutions", "16/02/2026 08:28", "17/02/2026 08:28", "study math", false);
+		SubTask subTask = new SubTask("Note solutions", "16/02/2026 08:28", "17/02/2026 08:28", "study math", false);
 		
 		System.out.println(mainTask);
 		System.out.println();
