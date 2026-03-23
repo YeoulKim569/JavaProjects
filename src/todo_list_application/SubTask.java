4c70d2b9782c35c72e8131548ea1a95c99d5efbf
YeoulKim569 <yeoul421@gmail.com>
2026-03-23 20:21:01 +0800

    subtask implements serializable

--- src/todo_list_application/SubTask.java
+++ src/todo_list_application/SubTask.java
@@ -1,10 +1,11 @@
 package todo_list_application;
 
+import java.io.Serializable;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class SubTask {
+public class SubTask implements Serializable {
 	String title;
 	Date date;
 	Date deadline;
