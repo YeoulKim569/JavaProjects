ba92713ef662a82e2428d86e72157377c3882b35
YeoulKim569 <yeoul421@gmail.com>
2026-04-13 20:55:52 +0900

    display profile picture

--- src/my_portfolio/ProfileUI.java
+++ src/my_portfolio/ProfileUI.java
@@ -1,14 +1,22 @@
 package my_portfolio;
+import java.awt.BorderLayout;
+
 import javax.swing.*;
 
 public class ProfileUI extends JFrame {
 	public ProfileUI() {
-		Profile profile = new Profile();
+		Profile profile = new Profile("Kim Yeoul", 18, "High School Graduate", "Journaling", "pictures/profile_picture.jpeg" );
 		
 		setTitle("Profile");
 	    setSize(500, 700);
 	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    setLocationRelativeTo(null);	
+	    
+	    ImageIcon imageIcon = new ImageIcon(profile.getImagePath()); // <-- your JPG file
+        JLabel imageLabel = new JLabel(imageIcon);
+        imageLabel.setHorizontalAlignment(JLabel.LEFT);
+        
+        add(imageLabel, BorderLayout.NORTH);
 	    
 	    setVisible(true);
 	}
